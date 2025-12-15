fe5317f15a7835eae1cea4aca032bf56b978ff3b
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-12-15 14:08:07 +0900

    update: 패키지 경로 수정

--- src/main/kotlin/team/hanseungil/nochu/domain/auth/dto/AuthResponse.kt
+++
@@ -1,5 +0,0 @@
-package team.hanseungil.nochu.domain.auth.dto
-
-data class AuthResponse(
-    val memberId: Long,
-)
---
+++ src/main/kotlin/team/hanseungil/nochu/domain/auth/presentation/dto/AuthResponse.kt
@@ -0,0 +1,5 @@
+package team.hanseungil.nochu.domain.auth.presentation.dto
+
+data class AuthResponse(
+    val memberId: Long,
+)
