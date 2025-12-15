87d6a4c624d0cbf56ad5be4456fc75ec32c84ee8
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-12-15 16:41:32 +0900

    add: EmotionWebClient 추가

---
+++ src/main/kotlin/team/hanseungil/nochu/domain/emotion/presentation/dto/response/AnalyzeEmotionResponse.kt
@@ -0,0 +1,16 @@
+package team.hanseungil.nochu.domain.emotion.presentation.dto.response
+
+data class AnalyzeEmotionResponse(
+    val emotions: Emotions,
+    val emotion: String,
+    val comment: String,
+) {
+    data class Emotions(
+        val happy: Double,
+        val surprise: Double,
+        val anger: Double,
+        val anxiety: Double,
+        val hurt: Double,
+        val sad: Double,
+    )
+}
