d70db8d2e06d9e3c5d59d328f6b49b3b8e08790a
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-12-15 12:27:47 +0900

    update: 테스트 코드 수정

--- src/test/kotlin/team/hanseungil/nochu/domain/auth/service/SignUpServiceTest.kt
+++ src/test/kotlin/team/hanseungil/nochu/domain/auth/service/SignUpServiceTest.kt
@@ -1,5 +1,6 @@
 package team.hanseungil.nochu.domain.auth.service
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -8,18 +9,21 @@ import io.mockk.verify
 import org.springframework.security.crypto.password.PasswordEncoder
 import team.hanseungil.nochu.domain.member.entity.Member
 import team.hanseungil.nochu.domain.member.repository.MemberJpaRepository
+import team.hanseungil.nochu.global.error.ErrorCode
+import team.hanseungil.nochu.global.error.GlobalException
 
 class SignUpServiceTest : BehaviorSpec({
-    val memberJpaRepository = mockk<MemberJpaRepository>()
-    val passwordEncoder = mockk<PasswordEncoder>()
-    val signUpService = SignUpService(memberJpaRepository, passwordEncoder)
-
-    Given("회원가입 요청이 주어졌을 때") {
+    Given("유효한 닉네임과 비밀번호가 주어졌을 때") {
+        val memberJpaRepository = mockk<MemberJpaRepository>()
+        val passwordEncoder = mockk<PasswordEncoder>()
+        val signUpService = SignUpService(memberJpaRepository, passwordEncoder)
+        
         val nickname = "testuser"
         val password = "password123"
         val encodedPassword = "encodedPassword"
         val savedMember = Member(id = 1L, nickname = nickname, password = encodedPassword)
 
+        every { memberJpaRepository.existsByNickname(nickname) } returns false
         every { passwordEncoder.encode(password) } returns encodedPassword
         every { memberJpaRepository.save(any()) } returns savedMember
 
@@ -29,10 +33,25 @@ class SignUpServiceTest : BehaviorSpec({
             Then("memberId를 반환한다") {
                 result shouldBe 1L
             }
+        }
+    }
 
-            Then("비밀번호가 암호화되어 저장된다") {
-                verify(exactly = 1) { passwordEncoder.encode(password) }
-                verify(exactly = 1) { memberJpaRepository.save(match { it.password == encodedPassword }) }
+    Given("이미 존재하는 닉네임이 주어졌을 때") {
+        val memberJpaRepository = mockk<MemberJpaRepository>()
+        val passwordEncoder = mockk<PasswordEncoder>()
+        val signUpService = SignUpService(memberJpaRepository, passwordEncoder)
+        
+        val nickname = "existingUser"
+        val password = "password123"
+
+        every { memberJpaRepository.existsByNickname(nickname) } returns true
+
+        When("회원가입을 실행하면") {
+            Then("MEMBER_NICKNAME_ALREADY_EXISTS 예외가 발생한다") {
+                val exception = shouldThrow<GlobalException> {
+                    signUpService.execute(nickname, password)
+                }
+                exception.errorCode shouldBe ErrorCode.MEMBER_NICKNAME_ALREADY_EXISTS
             }
         }
     }
