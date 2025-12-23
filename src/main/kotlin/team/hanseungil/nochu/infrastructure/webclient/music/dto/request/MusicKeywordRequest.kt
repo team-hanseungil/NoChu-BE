65a755d776209b6dbe70ef80faf1c5bc31ff9e16
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-12-23 10:36:26 +0900

    update: 요청 dto 수정

--- src/main/kotlin/team/hanseungil/nochu/infrastructure/webclient/music/dto/request/MusicKeywordRequest.kt
+++ src/main/kotlin/team/hanseungil/nochu/infrastructure/webclient/music/dto/request/MusicKeywordRequest.kt
@@ -1,14 +1,10 @@
 package team.hanseungil.nochu.infrastructure.webclient.music.dto.request
 
 data class MusicKeywordRequest(
-    val emotions: Emotions,
-) {
-    data class Emotions(
-        val happy: Double,
-        val surprise: Double,
-        val anger: Double,
-        val anxiety: Double,
-        val hurt: Double,
-        val sad: Double,
-    )
-}
+    val happy: Double,
+    val surprise: Double,
+    val anger: Double,
+    val anxiety: Double,
+    val hurt: Double,
+    val sad: Double,
+)
