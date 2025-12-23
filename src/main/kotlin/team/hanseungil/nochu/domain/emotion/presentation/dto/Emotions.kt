ebc53c7e139ed6f617869f3f7b4ae928dee098d4
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-12-23 09:06:45 +0900

    add: Emotions dto 추가

---
+++ src/main/kotlin/team/hanseungil/nochu/domain/emotion/presentation/dto/Emotions.kt
@@ -0,0 +1,10 @@
+package team.hanseungil.nochu.domain.emotion.presentation.dto
+
+data class Emotions(
+    val happy: Double,
+    val surprise: Double,
+    val anger: Double,
+    val anxiety: Double,
+    val hurt: Double,
+    val sad: Double,
+)
