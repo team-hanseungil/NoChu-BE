194166540aa5d0e1b06a2de20e9d501c7e0e10bd
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-12-23 10:27:16 +0900

    update: emotion 필드 제거

--- src/main/kotlin/team/hanseungil/nochu/infrastructure/webclient/music/dto/request/MusicKeywordRequest.kt
+++ src/main/kotlin/team/hanseungil/nochu/infrastructure/webclient/music/dto/request/MusicKeywordRequest.kt
@@ -2,7 +2,6 @@ package team.hanseungil.nochu.infrastructure.webclient.music.dto.request
 
 data class MusicKeywordRequest(
     val emotions: Emotions,
-    val emotion: String,
 ) {
     data class Emotions(
         val happy: Double,
